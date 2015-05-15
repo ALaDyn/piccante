c0b13290d624b46461fb9770143a0e3a1c4dc06a
Stefano Sinigardi <stesinigardi@hotmail.com>
2015-05-15 16:06:54 +0200

    add CINECA's galileo makefile

---
+++ compile_on_galileo.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+module purge
+module load profile/advanced
+module load intel/cs-xe-2015--binary
+module load intelmpi/5.0.2--binary
+module load boost/1.57.0--intel--cs-xe-2015--binary
+module load gsl/1.16--intel--cs-xe-2015--binary
+
+make -f makefile.galileo
