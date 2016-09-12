001f2638bdb34c0d937af7028fb50583232f1eca
Andrea Sgattoni <a.sgatto@gmail.com>
2016-09-12 17:00:17 +0200

    correct gnu scripts cnaf

--- build/cnaf.gnu.sh
+++ build/cnaf.gnu.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 module purge
-module load compilers/gcc-4.8.2
+module load compilers/gcc-4.9.0
+module load boost_1_56_0_gcc4_9_0
 module load compilers/openmpi-1.8.1_gcc-4.8.2
 
 make cnaf
