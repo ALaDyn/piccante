a36175cc94d7a74b55501feccbdb5f9567a13adc
Andrea Sgattoni <a.sgatto@gmail.com>
2016-10-10 15:38:11 +0200

    correct run script for cnaf-intel

--- run/cnaf_intel_144.sh
+++ run/cnaf_intel_144.sh
@@ -3,7 +3,7 @@
 
 NUMERO_TOTALE_CORE_DA_USARE=144
 
-NOME_ESEGUIBILE="./piccante"
+NOME_ESEGUIBILE="./piccante.exe"
 stderr_file=epic.txt
 stdout_file=opic.txt
 
@@ -24,7 +24,6 @@ echo "#BSUB -o %J.out" >> $job # Job standard output
 echo "#BSUB -e %J.err" >> $job # Job standard error
 echo "#BSUB -q ${queue}" >> $job
 echo "#BSUB -n ${NUMERO_TOTALE_CORE_DA_USARE}" >> $job
-echo "module load compilers/gcc-4.8.2" >> $job
 echo "module load compilers/intel-parallel-studio-2016" >> $job
 echo "module load boost_1_56_0_gcc4_9_0" >> $job
 echo "/shared/software/compilers/impi/intel64/bin/mpirun -np ${NUMERO_TOTALE_CORE_DA_USARE} -genv PSM_SHAREDCONTEXTS_MAX 8 -genv I_MPI_FABRICS shm:tmi ${NOME_ESEGUIBILE} >> ${stdout_file} 2>> ${stderr_file}" >> $job
