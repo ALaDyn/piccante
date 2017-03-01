740f1a43e2deac01d1203bb66bd2ce4728ad0b00
Andrea Sgattoni <a.sgatto@gmail.com>
2017-03-01 16:44:12 +0100

    correct run script cnaf intel

--- run/cnaf_intel_144.sh
+++ run/cnaf_intel_144.sh
@@ -24,6 +24,8 @@ echo "#BSUB -o %J.out" >> $job # Job standard output
 echo "#BSUB -e %J.err" >> $job # Job standard error
 echo "#BSUB -q ${queue}" >> $job
 echo "#BSUB -n ${NUMERO_TOTALE_CORE_DA_USARE}" >> $job
+echo "#BSUB -R \"span[ptile=32]\"" >> $job
+echo "export TMI_CONFIG=/shared/software/compilers/impi/intel64/etc/tmi.conf" >> $job
 echo "module load compilers/intel-parallel-studio-2016" >> $job
 echo "module load boost_1_56_0_gcc4_9_0" >> $job
 echo "/shared/software/compilers/impi/intel64/bin/mpirun -np ${NUMERO_TOTALE_CORE_DA_USARE} -genv PSM_SHAREDCONTEXTS_MAX 8 -genv I_MPI_FABRICS shm:tmi ${NOME_ESEGUIBILE} >> ${stdout_file} 2>> ${stderr_file}" >> $job
